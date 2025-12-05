74c727a81f9e00d3c6ab1685c84861d30a036d1b
SWAYAM GUPTA <gswayam94@gmail.com>
2025-12-05 19:19:12 +0530

    Update logger.py

--- bot/logger.py
+++ bot/logger.py
@@ -1,1 +1,12 @@
 
+import logging
+
+LOG = logging.getLogger("telegram_groq_bot")
+LOG.setLevel(logging.INFO)
+
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter(
+    "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+))
+
+LOG.addHandler(handler)
